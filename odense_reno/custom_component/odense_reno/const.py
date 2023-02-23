e8089dc225b2ff04462854780a1ea71d36dc249b
fars-fede-fire <87006332+fars-fede-fire@users.noreply.github.com>
2023-02-23 14:44:23 +0100

    Delete odense_reno directory

--- odense_reno/custom_component/odense_reno/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for odense_reno."""
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-NAME = "Odense Renovation"
-DOMAIN = "odense_reno"
-VERSION = "0.1.0"
