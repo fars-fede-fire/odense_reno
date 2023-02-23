65150f242e9b94c4e642822bf224de4c54c48e5f
fars-fede-fire <87006332+fars-fede-fire@users.noreply.github.com>
2023-02-23 14:42:59 +0100

    First  commit

---
+++ odense_reno/custom_component/odense_reno/const.py
@@ -0,0 +1,8 @@
+"""Constants for odense_reno."""
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+NAME = "Odense Renovation"
+DOMAIN = "odense_reno"
+VERSION = "0.1.0"
